aae73a93204292b043252d1f0421f2eeade8c496
Matthew Brett <matthew.brett@gmail.com>
2017-07-12 21:10:13 +0100

    We don't need msys2 for the scipy build

--- install_mingwpy.bat
+++ install_mingwpy.bat
@@ -1,12 +1,6 @@
 REM Install mingwpy into matching bitness Python 2.7, patch, put on PATH
 REM Expects (default)
 REM    PYTHON_ARCH: 64 or 32
-REM    MSYS2_ROOT: c:\msys64
-REM put msys2 at end of PATH, but strip out gcc, gfortran
-IF [%MSYS2_ROOT%] EQU [] set MSYS2_ROOT=c:\msys64
-set PATH=%PATH%;%MSYS2_ROOT%
-pacman -Sy --noconfirm git zip
-pacman -Rs --noconfirm gcc gcc-fortran
 REM install and patch mingwpy
 set MINGW_PYTHON=C:\Python27
 IF [%PYTHON_ARCH%] EQU [64] set MINGW_PYTHON=%MINGW_PYTHON%-x64
