1db477240243ec54802975027e687bd781dce051
Matthew Brett <matthew.brett@gmail.com>
2017-07-13 00:21:29 +0100

    Fix patching of specs file

--- patch_mingwpy.sh
+++ patch_mingwpy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # patch mingwpy for msvcrt builds
-MY_DIR=$(dirname "${BASH_SOURCE[0]}")
-# Patch specs file
-patch_file="${MY_DIR}/specs.patch"
+patch_file="${PWD}/specs.patch"
 specs_dir=$(dirname $(gcc --print-file specs))
 (cd $specs_dir && patch < $patch_file)
 gcc -dumpspecs
