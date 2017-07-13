a14b8a03e4d649c4fb197bb55a5da76270ec9f33
Matthew Brett <matthew.brett@gmail.com>
2017-07-13 20:41:55 +0100

    More debug prints

--- patch_mingwpy.sh
+++ patch_mingwpy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # patch mingwpy for msvcrt builds
 patch_file="${PWD}/specs.patch"
+echo One $(gcc --print-file specs)
+echo Two $(dirname $(gcc --print-file specs))
 specs_dir=$(dirname $(gcc --print-file specs))
 echo Here $specs_dir
 (cd ${specs_dir} && echo $PWD && patch -p0 < $patch_file)
