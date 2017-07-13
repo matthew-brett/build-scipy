ce859d983b4e7c30acb1421114c3a8198cf62cad
Matthew Brett <matthew.brett@gmail.com>
2017-07-13 20:39:23 +0100

    Debug for failing specs patch

--- install_mingwpy.bat
+++ install_mingwpy.bat
@@ -12,4 +12,6 @@ if [%PYTHON_ARCH%]==[64] (
 )
 set PATH=%MINGW_PYTHON%\Scripts;%PATH%
 %MINGW_PYTHON%\Scripts\pip.exe install -i https://pypi.anaconda.org/carlkl/simple mingwpy
+where gcc
+gcc --print-file specs
 cmd /c run_bash patch_mingwpy.sh
--- patch_mingwpy.sh
+++ patch_mingwpy.sh
@@ -2,5 +2,6 @@
 # patch mingwpy for msvcrt builds
 patch_file="${PWD}/specs.patch"
 specs_dir=$(dirname $(gcc --print-file specs))
-(cd $specs_dir && echo $PWD && patch -p0 < $patch_file)
+echo Here $specs_dir
+(cd ${specs_dir} && echo $PWD && patch -p0 < $patch_file)
 gcc -dumpspecs
