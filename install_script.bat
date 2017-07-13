9a4b93394c523e0ac2d2c24452491d5a2fb46a75
Matthew Brett <matthew.brett@gmail.com>
2017-07-13 19:11:28 +0100

    More adaptation of scripts

--- install_script.bat
+++ install_script.bat
@@ -8,7 +8,7 @@ REM https://stackoverflow.com/questions/1746475/windows-batch-help-in-setting-a-
 set PY_CMD=%PYTHON%\Python.exe -c "import platform; print(platform.architecture()[0][:2])"
 for /f "tokens=1 delims=" %%i in ('%PY_CMD%') do set PYTHON_ARCH=%%i
 REM Install mingwpy into matching bitness Python 2.7, patch, put on PATH
-install_mingwpy.bat
+call install_mingwpy.bat
 REM Put the building Python on the path
 SET PATH=%PYTHON%;%PYTHON%\Scripts;%PATH%
 REM Download built openblas libraries
@@ -19,8 +19,7 @@ REM Update pip, virtualenv
 REM Build in virtualenv
 if exist venv (rmdir /s /q venv)
 %PYTHON%\Scripts\virtualenv venv
-venv\Scripts\activate.bat
-where python
+call venv\Scripts\activate.bat
 REM OpenBLAS numpy wheels
 pip install --no-index -f %OPENBLAS_WHEEL_URL% numpy
 REM Patch numpy distutils
