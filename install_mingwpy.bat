7670029030079045858aac46541f68ed48cad006
Matthew Brett <matthew.brett@gmail.com>
2017-07-13 18:47:04 +0100

    Refactor scripts to run outside Appveyor

--- install_mingwpy.bat
+++ install_mingwpy.bat
@@ -1,9 +1,14 @@
 REM Install mingwpy into matching bitness Python 2.7, patch, put on PATH
 REM Expects (default)
 REM    PYTHON_ARCH: 64 or 32
+REM    PYMW_64: c:\Python27-64
+REM    PYMW_32: c:\Python27
 REM install and patch mingwpy
-set MINGW_PYTHON=C:\Python27
-IF [%PYTHON_ARCH%] EQU [64] set MINGW_PYTHON=%MINGW_PYTHON%-x64
+if [%PYTHON_ARCH%]==[64] (
+    set MINGW_PYTHON=%PYMW_64%
+) else (
+    set MINGW_PYTHON=%PYMW_32%
+)
 set PATH=%MINGW_PYTHON%\Scripts;%PATH%
 %MINGW_PYTHON%\Scripts\pip.exe install -i https://pypi.anaconda.org/carlkl/simple mingwpy
 bash patch_mingwpy.sh
--- prepare_local.bat
+++ prepare_local.bat
@@ -3,4 +3,6 @@ set SCIPY_COMMIT=master
 set OPENBLAS_COMMIT=v0.2.19
 set OPENBLAS_ROOT=c:\opt
 set OPENBLAS_WHEEL_URL=https://6faba06bebdc7f28a358-9ba0e05766e4bd68e53ad28ecdbfcaa7.ssl.cf2.rackcdn.com
-set PYTHON=C:\Python36-x64
+set PYTHON=C:\Python361-64
+set PYMW_32=C:\Python27
+set PYMW_64=C:\Python27-64
