631ad834a134bf82867ebaeb616420ebb9decf92
Matthew Brett <matthew.brett@gmail.com>
2017-07-13 20:53:06 +0100

    The problem was we were picking up msys2 gcc

--- patch_mingwpy.sh
+++ patch_mingwpy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # patch mingwpy for msvcrt builds
 patch_file="${PWD}/specs.patch"
-echo Zero $(which gcc)
-echo Zero.One ${PWD} $(gcc --version)
-echo Zero.Two $(ls)
-echo One $(gcc --print-file specs)
-echo Two $(dirname $(gcc --print-file specs))
 specs_dir=$(dirname $(gcc --print-file specs))
-echo Here $specs_dir
-(cd ${specs_dir} && echo $PWD && patch -p0 < $patch_file)
+(cd ${specs_dir} && patch -p0 < $patch_file)
 gcc -dumpspecs
