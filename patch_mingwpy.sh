8a1b5dbb175efa14b14748e81c9d90bb3e7df01a
Matthew Brett <matthew.brett@gmail.com>
2017-07-14 09:43:12 +0100

    Add mingw32, mingwex libraries

--- patch_mingwpy.sh
+++ patch_mingwpy.sh
@@ -5,7 +5,10 @@ patch_file="${PWD}/specs.patch"
 specs_dir=$(dirname $(gcc --print-file specs))
 (cd ${specs_dir} && \
     patch -p0 < $patch_file && \
+    cp  ../../../../*-mingw32/lib/libmingw*.a . && \
+    cp libmingw32.a mingw32.lib  && \
+    cp libmingwex.a mingwex.lib  && \
+    cp libgcc.a gcc.lib  && \
     cp libgfortran.a gfortran.lib && \
-    cp libquadmath.a quadmath.lib && \
-    cp libgcc.a gcc.lib)
+    cp libquadmath.a quadmath.lib)
 gcc -dumpspecs
